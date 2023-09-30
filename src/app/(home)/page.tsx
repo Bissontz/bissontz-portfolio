61008379f7a099f6d9cd6c791dc43e13ef219302
John Bissontz <47512933+BissontzJ@users.noreply.github.com>
2023-09-30 01:24:39 -0400

    escaped apostraphe

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -4,7 +4,7 @@ import Layout from "../components/layout";
 const Home = () => (
     <Layout>
         <section id='hero' className="text-center">
-            <h1 className="text-3xl font-semibold">Hello, I'm John Bissontz</h1>
+            <h1 className="text-3xl font-semibold">Hello, I&apos;m John Bissontz</h1>
             <p className="mt-2">A passionate and creative professional.</p>
             <a href='#contact' className="mt-4 inline-block bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 transition duration-300">Contact Me</a>
         </section>
