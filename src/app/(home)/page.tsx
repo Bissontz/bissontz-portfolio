623bd3e8ebe66c3969744bb6d3aa21c25720c8a9
John Bissontz <47512933+BissontzJ@users.noreply.github.com>
2023-10-01 00:29:21 -0400

    Apostraphe fix again

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -5,7 +5,7 @@ const Home = () => (
     <Layout>
         <section id='hero' className="text-justify">
             <h1 className="text-5xl font-semibold px-10">Hello, </h1>
-            <h1 className="text-5xl font-semibold px-10">I'm John Bissontz!</h1>
+            <h1 className="text-5xl font-semibold px-10">I&apos;m John Bissontz!</h1>
             <p className="mt-2 px-10">Full-stack Developer</p>
             {/*<a href='#contact' className="mt-4 inline-block bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 transition duration-300">Contact Me</a>*/}
         </section>
