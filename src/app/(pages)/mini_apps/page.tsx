838e4d763e2f2b8c422df84c8a846eb850d4549b
John Bissontz <47512933+BissontzJ@users.noreply.github.com>
2023-11-20 22:25:44 -0500

    Trying to put the button back

--- src/app/(pages)/mini_apps/page.tsx
+++ src/app/(pages)/mini_apps/page.tsx
@@ -11,7 +11,7 @@ const MiniAppsPage = () => (
         <div className="divider"/>
         <div className="grid gap-4 grid-cols-2">
             <button className="btn">Color Guesser Game</button>
-            <Link href="mini_apps/maze_runner">Maze Runner</Link>
+            <Link href="mini_apps/maze_runner"><button className="btn">Maze Runner</button></Link>
             <button className="btn">Mini App 3</button>
             <button className="btn">Mini App 4</button>
             <button className="btn">Mini App 5</button>
